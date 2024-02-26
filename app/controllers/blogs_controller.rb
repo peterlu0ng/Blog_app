5ff18215091ea31a89cabdf9c1aff38d4c58daf2
peterluong <peter.luong678@gmail.com>
2024-02-26 20:18:13 +1100

    added error message when creating new blog entry

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,32 +18,23 @@ class BlogsController < ApplicationController
 
   # POST /blogs or /blogs.json
   def create
-    @blog = Blog.new(params.require(:blog).permit(:title, :body))
-    @blog.save
-    redirect_to blogs_path(@blog)
+    @blog = Blog.new(blog_params)
+    if @blog.save #validation occurs if this fails
+        redirect_to @blog
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # PATCH/PUT /blogs/1 or /blogs/1.json
   def update
-    respond_to do |format|
-      if @blog.update(blog_params)
-        format.html { redirect_to blog_url(@blog), notice: "Blog was successfully updated." }
-        format.json { render :show, status: :ok, location: @blog }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
-      end
-    end
+
   end
 
   # DELETE /blogs/1 or /blogs/1.json
   def destroy
     @blog.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to blogs_url, notice: "Blog was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
