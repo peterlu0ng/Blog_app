7806733c312f0a4a3e5fbfc3395d9e9597471fa6
peterluong <peter.luong678@gmail.com>
2024-03-03 15:59:04 +1100

    sort listings in order of IDs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs or /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.all.sort
   end
 
   # GET /blogs/1 or /blogs/1.json
