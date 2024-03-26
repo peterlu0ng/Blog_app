e9b98421ad5601d4005580fa99e233c472639832
Peter Luong <peter.luong678@gmail.com>
2024-03-26 23:39:52 +1100

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Blog #{@user.username}, you have sucessfully signed up"
+      redirect_to blogs_path
+    else
+      render 'new'
+
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
