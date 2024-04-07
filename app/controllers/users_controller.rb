338e3dd93986ac605556c61f27844f005973549a
Peter Luong <peter.luong678@gmail.com>
2024-04-07 15:55:00 +1000

    add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @blogs = @user.blogs
+  end
   def new
     @user = User.new
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 } )
+  # get the email from URL-parameters or what have you and make lowercase
+  email_address = user.email.downcase
+  # create the md5 hash
+  hash = Digest::MD5.hexdigest(email_address)
+  size = options[:size]
+  # compile URL which can be used in <img src="RIGHT_HERE"...
+  gravatar_url = image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+  image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+
+  end
 end
