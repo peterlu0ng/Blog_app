ef16e3d43737b52196203dddf0a8376beb872d71
Peter Luong <peter.luong678@gmail.com>
2024-04-07 15:18:32 +1000

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was updated successfully"
+      redirect_to blogs_path
+    else
+      render 'edit'
+    end
+  end
+
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -9,7 +25,6 @@ class UsersController < ApplicationController
       redirect_to blogs_path
     else
       render 'new'
-
     end
   end
   private
