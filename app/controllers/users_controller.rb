8e40fc7d8cec41f7df1fde09942a248b85f0722e
Peter Luong <peter.luong678@gmail.com>
2024-04-14 21:28:07 +1000

    delete account and all associated blogs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_user, except: [ :show, :index ]
-  before_action :require_same_user, only: [ :edit, :update ]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [ :edit, :update ]
+  before_action :require_same_user, only: [ :edit, :update, :destroy ]
 
 
 
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
     end
   end
 
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -40,6 +39,13 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+    def destroy
+      @user.destroy
+      session[:user_id] = nil
+      flash[:notice] = "Account and all associated blogs have been deleted successfully"
+      redirect_to root_path
+    end
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
