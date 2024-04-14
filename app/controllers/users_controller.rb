686ff36c3e0442768591dc637e51610bf51e45cf
Peter Luong <peter.luong678@gmail.com>
2024-04-14 16:47:27 +1000

    login users when creating an account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
     @blogs = @user.blogs.paginate(page: params[:page], per_page: 3)
 
   end
@@ -13,11 +13,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was updated successfully"
       redirect_to user_path
@@ -30,6 +28,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Blog #{@user.username}, you have sucessfully signed up"
       redirect_to blogs_path
     else
@@ -40,4 +39,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
