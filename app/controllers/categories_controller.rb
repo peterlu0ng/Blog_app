085157c18517e751f1f20f8de11f58ebfc5889d4
Peter Luong <peter.luong678@gmail.com>
2024-04-22 11:16:32 +1000

    add admin action to create and edit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def new
     @category = Category.new
@@ -12,9 +13,11 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
+
   def show
     @category = Category.find(params[:id])
   end
@@ -22,5 +25,11 @@ class CategoriesController < ApplicationController
   private
   def category_params
     params.require(:category).permit(:name)
+  end
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = 'Only admins can perform that action'
+      redirect_to categories_path
+    end
   end
 end
