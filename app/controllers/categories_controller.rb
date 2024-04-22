f2316156d23ae9483778a3803ffb52b011ca9a3d
Peter Luong <peter.luong678@gmail.com>
2024-04-22 10:47:41 +1000

    added categories listing and pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,7 +13,7 @@ class CategoriesController < ApplicationController
     end
   end
   def index
-
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
   def show
     @category = Category.find(params[:id])
