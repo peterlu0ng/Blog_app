da3dd2841770c2c0c0c74682f376bed9db04566d
Peter Luong <peter.luong678@gmail.com>
2024-04-22 12:23:42 +1000

    create many-to-many associations in backend

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  has_many :blog_categories
+  has_many :categories, through: :blog_categories
   validates :title, presence: true, length: {minimum: 6, maximum: 100}
   validates :body, presence: true, length: {minimum: 10, maximum: 300}
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+  has_many :blog_categories
+  has_many :blogs, through: :blog_categories
   validates :name, presence: true, uniqueness: true, length: {minimum: 3, maximum:25}
 end
